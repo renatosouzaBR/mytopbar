4686cc0e4c9b81361e59bea33cc0205148af5efb
Renato Souza <renatosouza@pop-os.localdomain>
2021-11-23 10:05:51 -0300

    Remove pasta lib

--- lib/esm/index.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="react" />
-import "./style.css";
-interface Props {
-    backgroundColor: string;
-    height: number;
-    maxWidth: number;
-}
-export declare const MyTopbar: ({ backgroundColor, height, maxWidth, }: Props) => JSX.Element;
-export {};
