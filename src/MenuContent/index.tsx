75b9168dc85d9e5da1742e43377b492d96f2525b
Renato Souza <renatosouza@pop-os.localdomain>
2021-11-27 17:44:56 -0300

    Adiciona icone de menu hambuger para responsividade

--- src/MenuContent/index.tsx
+++ src/MenuContent/index.tsx
@@ -1,6 +1,9 @@
 import React, { CSSProperties } from "react";
 import "./styles.css";
+import "./responsive.css";
 import { MenuContentProps } from "./types";
+
+import { ReactComponent as HamburgerIcon } from "../assets/hamburger-icon.svg";
 
 export const MenuContent: React.FC<MenuContentProps> = ({ menu }) => {
   const liStyle = (textColor: string, borderColor: string): CSSProperties => ({
@@ -9,37 +12,40 @@ export const MenuContent: React.FC<MenuContentProps> = ({ menu }) => {
   });
 
   return (
-    <ul id="menu">
-      {menu.map((item) => {
-        return (
-          <li
-            className={item.submenu && "menu-hover"}
-            style={
-              item.style &&
-              liStyle(item.style?.textColor, item.style?.borderColor)
-            }
-            onClick={item.onClick}
-          >
-            {item.label}
+    <>
+      <HamburgerIcon id="hamburger-icon" />
+      <ul id="menu">
+        {menu.map((item) => {
+          return (
+            <li
+              className={item.submenu && "menu-hover"}
+              style={
+                item.style &&
+                liStyle(item.style?.textColor, item.style?.borderColor)
+              }
+              onClick={item.onClick}
+            >
+              {item.label}
 
-            {item.submenu && (
-              <ul className="submenu">
-                {item.submenu.map((subitem) => (
-                  <li
-                    style={
-                      item.style &&
-                      liStyle(item.style?.textColor, item.style?.borderColor)
-                    }
-                    onClick={subitem.onClick}
-                  >
-                    {subitem.label}
-                  </li>
-                ))}
-              </ul>
-            )}
-          </li>
-        );
-      })}
-    </ul>
+              {item.submenu && (
+                <ul className="submenu">
+                  {item.submenu.map((subitem) => (
+                    <li
+                      style={
+                        item.style &&
+                        liStyle(item.style?.textColor, item.style?.borderColor)
+                      }
+                      onClick={subitem.onClick}
+                    >
+                      {subitem.label}
+                    </li>
+                  ))}
+                </ul>
+              )}
+            </li>
+          );
+        })}
+      </ul>
+    </>
   );
 };
