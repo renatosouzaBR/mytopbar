746f507070df449d362ceae4a9d3eda8e245c9cd
Renato Souza <renatosouza@pop-os.localdomain>
2021-11-27 17:44:33 -0300

    Separa a responsividade da logo

--- src/LogoContent/index.tsx
+++ src/LogoContent/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./styles.css";
+import "./responsive.css";
 import { LogoContentProps } from "./types";
 
 export const LogoContent: React.FC<LogoContentProps> = ({ logo }) => {
